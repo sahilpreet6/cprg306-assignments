bcbef07973b742d5c6b747f8dd417286f6002d3c
sahilpreet6 <sahilpreetsingh3@edu.sait.ca>
2026-01-22 14:09:25 -0700

    Completed Week 2 assignment

---
+++ app/week-2/student.info.tsx
@@ -0,0 +1,12 @@
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Sahilpreet Singh</h1>
+            <p>
+                <a href="https://github.com/sahilpreet6/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+                    GitHub Repository
+                </a>
+            </p>
+        </div>
+    );
+}
