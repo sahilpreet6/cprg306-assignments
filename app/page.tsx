9501986de45fb20f81456b34ddd827dc8531ff82
sahilpreet6 <sahilpreetsingh3@edu.sait.ca>
2026-02-05 13:36:25 -0700

    created pages

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default function Home() {
       <p>
         <Link href="/week-3">Go to Week 3</Link>
       </p>
+       <p>
+        <Link href="/week-4">Go to Week 4</Link>
+      </p>
     </main>
   );
 }
